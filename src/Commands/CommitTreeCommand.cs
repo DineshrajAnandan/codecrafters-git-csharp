fd0143071a7a18e9971ed50dc6b4d633accccfc5
DineshrajAnandan <dineshrajanandan@gmail.com>
2025-11-02 17:12:23 +0530

    refactor: commit tree

--- src/Commands/CommitTreeCommand.cs
+++ src/Commands/CommitTreeCommand.cs
@@ -1,6 +1,4 @@
-using System.Text;
 using codecrafters_git.Commands.Base;
-using codecrafters_git.Helpers;
 using codecrafters_git.Models;
 using codecrafters_git.Models.Objects;
 
@@ -9,10 +7,6 @@ namespace codecrafters_git.Commands;
 [Command("commit-tree")]
 public class CommitTreeCommand: ICommand
 {
-    private const string AuthorName =  "John";
-    private const string AuthorEmail = "john@email.com";
-    private const string CommitterName =  "Smith";
-    private const string CommitterEmail =  "smith@email.com";
     
     public void Execute(string[] args)
     {
@@ -21,26 +15,8 @@ public class CommitTreeCommand: ICommand
         {
             throw new ArgumentException(@"Tree hash must be provided", nameof(tree));
         }
-
-        var sb = new StringBuilder();
-        sb.Append($"tree {tree}");
-
-        if (parent != null)
-        {
-            sb.Append($"\nparent {parent}");
-        }
-        var secondsSinceEpoch = new DateTimeOffset(DateTime.UtcNow).ToUnixTimeSeconds();
-        sb.Append($"\nauthor {AuthorName} <{AuthorEmail}> {secondsSinceEpoch} +0000");
-        sb.Append($"\ncommitter {CommitterName} <{CommitterEmail}> {secondsSinceEpoch} +0000");
-        sb.Append($"\n\n{message}\n");
         
-        var body = sb.ToString();
-        var content = $"commit {body.Length}\0{body}";
-        
-        var shaOne = ShaOne.CalculateFromText(content);
-        var commitObject = new CommitObject(shaOne);
-        FileHelper.CreateDirectories(commitObject.DirPath);
-        FileHelper.WriteAsZLib(commitObject.FilePath, content);
+        var shaOne = CommitObject.Create(tree, parent, message);
         Console.WriteLine(shaOne);
     }
 
--- src/Models/Objects/CommitObject.cs
+++ src/Models/Objects/CommitObject.cs
@@ -1,6 +1,32 @@
+using System.Text;
+using codecrafters_git.Helpers;
+
 namespace codecrafters_git.Models.Objects;
 
 public class CommitObject(ShaOne shaOne): GitObject(shaOne)
 {
-    
+    public static ShaOne Create(ShaOne tree, ShaOne? parent, string message)
+    {
+        var sb = new StringBuilder();
+        sb.Append($"tree {tree}");
+
+        if (parent != null)
+        {
+            sb.Append($"\nparent {parent}");
+        }
+        
+        var secondsSinceEpoch = new DateTimeOffset(DateTime.UtcNow).ToUnixTimeSeconds();
+        sb.Append($"\nauthor John <john@email.com> {secondsSinceEpoch} +0000");
+        sb.Append($"\ncommitter Smith <smith@email.com> {secondsSinceEpoch} +0000");
+        sb.Append($"\n\n{message}\n");
+        
+        var body = sb.ToString();
+        var content = $"commit {body.Length}\0{body}";
+        
+        var shaOne = ShaOne.CalculateFromText(content);
+        var commitObject = new CommitObject(shaOne);
+        FileHelper.CreateDirectories(commitObject.DirPath);
+        FileHelper.WriteAsZLib(commitObject.FilePath, content);
+        return shaOne;
+    }
 }
